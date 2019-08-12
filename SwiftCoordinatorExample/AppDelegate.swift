81430a3c533cb74377e965d2cadde7d35fc57244
George Gomes <georgegomees@gmail.com>
2019-08-12 17:25:40 -0300

    Setup project

---
+++ SwiftCoordinatorExample/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  SwiftCoordinatorExample
+//
+//  Created by George Gomes on 12/08/19.
+//  Copyright © 2019 George Gomes. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+}
+
