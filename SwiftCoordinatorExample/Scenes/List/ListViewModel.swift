bbdf3b6b6e6becf1acd58a9318212b042ac90112
George Gomes <georgegomees@gmail.com>
2019-08-14 08:34:12 -0300

    Reaload gitignore

---
+++ SwiftCoordinatorExample/Scenes/List/ListViewModel.swift
@@ -0,0 +1,9 @@
+//
+//  ListViewModel.swift
+//  SwiftCoordinatorExample
+//
+//  Created by George Gomes on 13/08/19.
+//  Copyright © 2019 George Gomes. All rights reserved.
+//
+
+import Foundation
